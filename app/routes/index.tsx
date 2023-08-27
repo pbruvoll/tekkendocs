c7db6c86f889edef272c93924e17abe7cd27254d
Philip Bruvoll <pbruvoll@hotmail.com>
2023-08-27 14:27:23 +0200

    Allow node 16 and up

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,33 +1,36 @@
 import { LoaderFunction, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { google } from "~/google.server";
 
 export const loader: LoaderFunction = async () => {
 
-  const target = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-  const jwt = new google.auth.JWT({
-    email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    scopes: target,
-    key: (process.env.GOOGLE_SHEETS_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-  });
+  // const target = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+  // const jwt = new google.auth.JWT({
+  //   email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+  //   scopes: target,
+  //   key: (process.env.GOOGLE_SHEETS_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  // });
 
-  const sheets = google.sheets({ version: 'v4', auth: jwt });
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: "1p-QCqB_Tb1GNX0KaicHr0tZwKa1taK5XeNvMr1N3D64",
-    range: 'landingpage', // sheet name
-  });
+  // const sheets = google.sheets({ version: 'v4', auth: jwt });
+  // const response = await sheets.spreadsheets.values.get({
+  //   spreadsheetId: "1p-QCqB_Tb1GNX0KaicHr0tZwKa1taK5XeNvMr1N3D64",
+  //   range: 'landingpage', // sheet name
+  // });
 
-  const rows = response.data.values;
+  //const rows = response.data.values;
 
-  return json({ rows, })
+  return json({})
 }
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to TekkenDocs</h1>
       <div>This site is under construction</div>
+      <h2>Characters</h2>
+      <ul>
+        <li><Link to="/nina">Nina</Link></li>
+      </ul>
     </div>
   );
 }
