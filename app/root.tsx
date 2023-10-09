28b309cac361707efda59ef24d616c90b04191f9
Philip Bruvoll <pbruvoll@hotmail.com>
2023-10-09 21:07:28 +0200

    remove the noindex to allow indexing site for search engines

--- app/root.tsx
+++ app/root.tsx
@@ -9,27 +9,26 @@ import {
 } from "@remix-run/react";
 import stylesUrl from "~/global.css";
 
-export const links = () => [
-  { rel: "stylesheet", href: stylesUrl },
-];
+export const links = () => [{ rel: "stylesheet", href: stylesUrl }];
 
 export const headers = () => ({
   "Cache-Control": "public, max-age=10, s-maxage=60",
 });
 
-export const meta: V2_MetaFunction = () => [{
-  title: "TekkenDocs | Frame data for Tekken",
-},
-{
-  description: "Frame data and usefull resources for Tekken"
-}];
+export const meta: V2_MetaFunction = () => [
+  {
+    title: "TekkenDocs | Frame data for Tekken",
+  },
+  {
+    description: "Frame data and usefull resources for Tekken",
+  },
+];
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="robots" content="noindex, nofollow" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
