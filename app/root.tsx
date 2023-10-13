d85f70de9eeb5413792d889833f012c6ab656491
Philip Bruvoll <pbruvoll@hotmail.com>
2023-10-14 00:16:24 +0200

    Add more icon

--- app/root.tsx
+++ app/root.tsx
@@ -38,6 +38,13 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <link rel="icon" sizes="512x512" href="/logo-512.png" />
+        <link rel="icon" sizes="256x256" href="/logo-256.png" />
+        <link rel="icon" sizes="128x128" href="/logo-128.png" />
+        <link rel="shortcut" href="/logo-128.png" />
+        <link rel="apple-touch-icon" href="logo-256.png" />
         <Meta />
         <Links />
       </head>
