e4ecb31cca70b23bd0c50d24955a0eb68820a8a4
Philip Bruvoll <pbruvoll@hotmail.com>
2024-02-04 18:43:25 +0100

    Add support for external resource for chars (#42)

---
+++ app/components/ResourcesTable.tsx
@@ -0,0 +1,45 @@
+import { Heading, Link as RadixLink, Table } from '@radix-ui/themes'
+import { Link } from '@remix-run/react'
+import { ContentContainer } from './ContentContainer'
+
+export const ResourcesTable = ({
+  rows,
+  headers,
+}: {
+  rows: string[][]
+  headers: string[]
+}) => {
+  return (
+    <section className="mt-8">
+      <ContentContainer>
+        <Heading as="h2" mb="4" size="4">
+          External Resources
+        </Heading>
+      </ContentContainer>
+      <Table.Root variant="surface" style={{ width: '100%' }}>
+        {headers && (
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+        )}
+        <Table.Body>
+          {rows.map((row, i) => {
+            return (
+              <Table.Row key={row[0]}>
+                <Table.Cell>
+                  <RadixLink asChild>
+                    <Link to={row[0]}>{row[1]}</Link>
+                  </RadixLink>
+                </Table.Cell>
+                <Table.Cell>{row[2]}</Table.Cell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table.Root>
+    </section>
+  )
+}
