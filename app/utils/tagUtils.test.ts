3caf0bd699acbc1759d88c2ec877715618722b57
Philip Bruvoll <pbruvoll@hotmail.com>
2024-04-01 18:45:02 +0200

    test

--- app/utils/tagUtils.test.ts
+++ app/utils/tagUtils.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest'
 import { tagStringToRecord } from './tagUtils'
 test('tagUtils', () => {
   expect(tagStringToRecord('pc, js9~11, cs11')).toEqual({
-    pc: undefined,
+    pc: "",
     js: '9~11',
     cs: '11',
   })
