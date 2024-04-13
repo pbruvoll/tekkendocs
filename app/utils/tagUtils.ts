d112ed25775a3b4cad08e820e9b68cf89462031a
Philip Bruvoll <pbruvoll@hotmail.com>
2024-04-13 18:41:26 +0200

    Better table headers

--- app/utils/tagUtils.ts
+++ app/utils/tagUtils.ts
@@ -1,7 +1,7 @@
 export const tagStringToRecord = (
   tagString: string,
 ): Record<string, string> => {
-  const tagValues = tagString.split(',')
+  const tagValues = tagString.split(' ')
   const tagValuePairs = tagValues.map(t => {
     const splitted = t.split(/(?<=[A-Za-z])(?=\d)/).map(s => s.trim()) // "pc8-11" => ["pc", "8-11"] and "pc" => ["pc", ""]
     splitted[1] = splitted[1] || ''
