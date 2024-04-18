bd3f1c94caecbeca5733dd75c5f2b739acf0711c
Philip Bruvoll <pbruvoll@hotmail.com>
2024-04-18 23:53:45 +0200

    Consolidate to json for api (#92)
    * reuse to json in api for tekken 8
    
    * fix
    
    * lint

--- app/routes/api.t8.$character.framedata.tsx
+++ app/routes/api.t8.$character.framedata.tsx
@@ -1,16 +1,16 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { hasHeaderMap } from '~/constants/hasHeaderMap'
-import { getSheet } from '~/services/googleSheetService.server'
+import { environment } from '~/constants/environment.server'
+import { SheetServiceMock } from '~/mock/SheetServiceMock'
+import { SheetServiceImpl } from '~/services/sheetServiceImpl.server'
 import type { Game } from '~/types/Game'
 import type { Move } from '~/types/Move'
-import type { Throw } from '~/types/Throw'
-import { cachified } from '~/utils/cache.server'
+import { type SheetService } from '~/types/SheetService'
+import { frameDataTableToJson } from '~/utils/frameDataUtils'
 import { getCacheControlHeaders } from '~/utils/headerUtils'
-import { sheetSectionToTable, sheetToSections } from '~/utils/sheetUtils.server'
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const character = params.character
-  if (!character) {
+  const characterId = params.character
+  if (!characterId) {
     throw new Response(null, {
       status: 400,
       statusText: 'Character cant be empty',
@@ -19,67 +19,21 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
   const game: Game = 'T8'
 
-  const key = `${character}|_|${game}`
-  const { sheet } = await cachified({
-    key,
-    ttl: 1000 * 30,
-    staleWhileRevalidate: 1000 * 60 * 60 * 24 * 3,
-    async getFreshValue() {
-      const sheet = await getSheet(character, game)
-      return { sheet }
-    },
-  })
-  if (!sheet) {
-    throw new Response(
-      `Not able to find data for character ${character} in game ${game}`,
-      { status: 500, statusText: 'server error' },
-    )
-  }
+  const service: SheetService = environment.useMockData
+    ? new SheetServiceMock()
+    : new SheetServiceImpl()
 
-  const { editUrl, rows } = sheet
-  const sheetSections = sheetToSections(rows)
-  const tables = sheetSections.map(ss =>
-    sheetSectionToTable({
-      name: ss.sectionId,
-      sheetSection: ss,
-      hasHeader: hasHeaderMap[ss.sectionId],
-    }),
+  const sheetData = await service.getCharacterData(
+    game,
+    characterId,
+    'frameData',
   )
-  const framesNormalTable = tables.find(t => t.name === 'frames_normal')
-  if (!framesNormalTable) {
-    throw json('Not able to find frame for character', { status: 500 })
-  }
 
-  const framesNormal = framesNormalTable.rows.map<Move>((row, index) => ({
-    moveNumber: index + 1,
-    command: row[0],
-    hitLevel: row[1],
-    damage: row[2],
-    startup: row[3],
-    block: row[4],
-    hit: row[5],
-    counterHit: row[6],
-    notes: row[7],
-  }))
+  const { tables, characterName, editUrl } = sheetData
 
-  const framesThrowsTable = tables.find(t => t.name === 'frames_throws')
-  const framesThrows = framesThrowsTable
-    ? framesThrowsTable.rows.map<Throw>(row => ({
-        command: row[0],
-        hitLevel: row[1],
-        damage: row[2],
-        startup: row[3],
-        hit: '?',
-        break: row[5],
-        breakCommand: row[4],
-        notes: row[6],
-      }))
-    : undefined
+  const normalMoves = tables.find(t => t.name === 'frames_normal')
+  const moves: Move[] = normalMoves ? frameDataTableToJson(normalMoves) : []
+  const data = { characterName, editUrl, game, framesNormal: moves }
 
-  return json(
-    { characterName: character, editUrl, framesNormal, framesThrows },
-    {
-      headers: getCacheControlHeaders({ seconds: 60 * 5 }),
-    },
-  )
+  return json(data, { headers: getCacheControlHeaders({ seconds: 60 * 5 }) })
 }
