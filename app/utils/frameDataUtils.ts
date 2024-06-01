94d9500faf94782140718dd6a9caf20f1cc71b08
Philip Bruvoll <pbruvoll@hotmail.com>
2024-06-02 01:22:22 +0200

    format

--- app/utils/frameDataUtils.ts
+++ app/utils/frameDataUtils.ts
@@ -448,30 +448,34 @@ export const sortMoves = (
 
 export const getStances = (moves: Move[]): Set<string> => {
   const allStances = moves.reduce((stanceSet, move) => {
-    if(move.hitLevel.startsWith("t")) { return stanceSet}
+    if (move.hitLevel.startsWith('t')) {
+      return stanceSet
+    }
     const stance = getStance(move.command)
     if (stance) {
       stanceSet.add(stance)
     }
     return stanceSet
   }, new Set<string>())
-  const stances = new Set(Array.from(allStances)
-  .filter(
-    stance =>
-      ![
-        'r',
-        'back',
-        'left',
-        'right',
-        'otg',
-        'r',
-        'ch',
-        'p',
-        '(during',
-        '(back',
-        '(face',
-      ].includes(stance.toLowerCase()) && !stance.includes(',')));
-  return stances;
+  const stances = new Set(
+    Array.from(allStances).filter(
+      stance =>
+        ![
+          'r',
+          'back',
+          'left',
+          'right',
+          'otg',
+          'r',
+          'ch',
+          'p',
+          '(during',
+          '(back',
+          '(face',
+        ].includes(stance.toLowerCase()) && !stance.includes(','),
+    ),
+  )
+  return stances
 }
 
 export const getStance = (command: string): string | undefined => {
