1d24f238fb63bd058d0c829d51ca66a41ed2a4f0
Philip Bruvoll <pbruvoll@hotmail.com>
2024-06-18 23:38:23 +0200

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev'
-import { remixDevTools } from "remix-development-tools";
+import { remixDevTools } from 'remix-development-tools'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
