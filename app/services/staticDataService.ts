663d3023a5484305335b251363aca37a170fd572
Philip Bruvoll <pbruvoll@hotmail.com>
2024-08-07 19:44:10 +0200

    update discord link

--- app/services/staticDataService.ts
+++ app/services/staticDataService.ts
@@ -108,7 +108,7 @@ export const getTekken8Characters = (): CharacterInfo[] => {
   return characterInfoListT8
 }
 
-export const discordInviteLink = 'https://discord.gg/aQHckBcS'
+export const discordInviteLink = 'https://discord.gg/eEuJHePy'
 export const githubLink = 'https://github.com/pbruvoll/tekkendocs'
 export const twitterLink = 'https://x.com/tekkendocs'
 export const facebooklink =
