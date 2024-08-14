368e88265ac567118fb0f871e03254e81b573924
Philip Bruvoll <pbruvoll@hotmail.com>
2024-08-14 22:21:25 +0200

    temporarly disable database (#171)

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from '@prisma/client'
-declare global {
-  var __prisma: PrismaClient
-}
-if (!global.__prisma) {
-  global.__prisma = new PrismaClient()
-}
-try {
-  global.__prisma.$connect()
-}
-catch (err) {
-  console.error('Unable to connect to the database:', err)
-}
-export const prisma = global.__prisma
+// import { PrismaClient } from '@prisma/client'
+// declare global {
+//   var __prisma: PrismaClient
+// }
+// if (!global.__prisma) {
+//   global.__prisma = new PrismaClient()
+// }
+// try {
+//   global.__prisma.$connect()
+// }
+// catch (err) {
+//   console.error('Unable to connect to the database:', err)
+// }
+// export const prisma = global.__prisma
--- app/routes/database-test.tsx
+++ app/routes/database-test.tsx
@@ -4,43 +4,44 @@ import {
   type LoaderFunctionArgs,
 } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
-import { prisma } from '~/db.server'
+//import { prisma } from '~/db.server'
 export async function action({ request }: ActionFunctionArgs) {
-  const count = await prisma.issue.count()
-  if (count > 100) {
-    return { success: false }
-  }
-  await prisma.issue.create({
-    data: {
-      title: 'Item title ' + Math.random(),
-      count: 1,
-      description: 'Item description',
-      draftId: Math.random().toString(),
-    },
-  })
+  // const count = await prisma.issue.count()
+  // if (count > 100) {
+  //   return { success: false }
+  // }
+  // await prisma.issue.create({
+  //   data: {
+  //     title: 'Item title ' + Math.random(),
+  //     count: 1,
+  //     description: 'Item description',
+  //     draftId: Math.random().toString(),
+  //   },
+  // })
   return {
     success: true,
   }
 }
 export async function loader({ request }: LoaderFunctionArgs) {
-  const data = await prisma.issue.findMany()
-  return json({
-    data,
-  })
+  // const data = await prisma.issue.findMany()
+  // return json({
+  //   data,
+  // })
+  return json({ data: [] })
 }
 export default function Index() {
-  const { data } = useLoaderData<typeof loader>()
+  // const { data } = useLoaderData<typeof loader>()
   return (
     <div>
       <h1> Items </h1>
-      <ul>
+      {/* <ul>
         {data.map(item => (
           <li key={item.id}>{item.title}</li>
         ))}
       </ul>
       <Form method="POST">
         <input type="submit" value="Submit" />
-      </Form>
+      </Form> */}
     </div>
   )
 }
