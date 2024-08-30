655e6676cae445a37dced7e7de414b5ce8c4eeb5
Philip Bruvoll <pbruvoll@hotmail.com>
2024-08-30 23:32:00 +0200

    update discord link

--- app/utils/getInTouch/discordContactProvider.ts
+++ app/utils/getInTouch/discordContactProvider.ts
@@ -3,7 +3,7 @@ import { type GetInTouchProvider } from './interfaces/GetInTouchProvider'
 type DiscordChannel = 'invite'
 
 const discordChannelMap: Record<DiscordChannel, string> = {
-  invite: 'https://discord.gg/eEuJHePy',
+  invite: 'https://discord.gg/Fxw33cbQ',
 }
 
 export class DiscordContactProvider
