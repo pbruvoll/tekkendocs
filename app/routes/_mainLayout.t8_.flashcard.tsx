6fe34b709a3346463e4b7207f4d59c63c75387a7
tonyly98 <tony.ly619@gmail.com>
2024-09-09 02:10:24 -0400

    indentation spacing to 2

--- app/routes/_mainLayout.t8_.flashcard.tsx
+++ app/routes/_mainLayout.t8_.flashcard.tsx
@@ -1,60 +1,60 @@
 import { json, type MetaFunction, type TypedResponse } from '@remix-run/node'
 import {
-    useLoaderData
+  useLoaderData
 } from '@remix-run/react'
 import { CharacterGrid } from '~/components/CharacterGrid'
 import { ContentContainer } from '~/components/ContentContainer'
 import {
-    getTekken8Characters
+  getTekken8Characters
 } from '~/services/staticDataService'
 import type { GamePageData } from '~/types/GamePageData'
 import { generateMetaTags } from '~/utils/seoUtils'
 import { t8AvatarMap } from '~/utils/t8AvatarMap'
 
 export const meta: MetaFunction = ({ matches }) => {
-    return generateMetaTags({
-        matches,
-        title: 'TekkenDocs | Flash cards',
-        description: 'Flash cards for Tekken 8',
-        image: { url: `/images/tekkendocs-og-image-v2.png` },
-        url: `/t8/flashcard`,
-    })
+  return generateMetaTags({
+    matches,
+    title: 'TekkenDocs | Flash cards',
+    description: 'Flash cards for Tekken 8',
+    image: { url: `/images/tekkendocs-og-image-v2.png` },
+    url: `/t8/flashcard`,
+  })
 }
 
 type LoaderData = {
-    gamePageDataT8: GamePageData
+  gamePageDataT8: GamePageData
 }
 export const loader = async (): Promise<TypedResponse<LoaderData>> => {
-    return json<LoaderData>(
-        {
-            gamePageDataT8: { characterInfoList: getTekken8Characters() },
-        }
-    )
+  return json<LoaderData>(
+    {
+      gamePageDataT8: { characterInfoList: getTekken8Characters() },
+    }
+  )
 }
 
 export default function () {
-    const { gamePageDataT8 } = useLoaderData<typeof loader>()
-    return (
-        <ContentContainer enableBottomPadding enableTopPadding>
-            <h1 className="my-4 text-2xl">Tekken 8 flash cards</h1>
-            <section>
-                <h2 className="my-3 text-xl">How it works</h2>
-                <p className="mb-2">
-                    A flash card shows a move on the front side. Your job is to guess a
-                    property of the move, for example how many frames it is on block. Then
-                    you flip the card and check if your guess was correct. Cards marked as
-                    "Wrong" will be shown again sooner than cards marked as "Correct".
-                    Cards marked as "Ignore" will never be shown again.
-                </p>
-            </section>
-            <CharacterGrid
-                characterCards={gamePageDataT8.characterInfoList.map(
-                    ({ id, displayName }) => {
-                        const imgSrc = t8AvatarMap[id]
-                        return { name: displayName, imgSrc, url: '/t8/' + id + '/flashcard' }
-                    },
-                )}
-            />
-        </ContentContainer>
-    )
+  const { gamePageDataT8 } = useLoaderData<typeof loader>()
+  return (
+    <ContentContainer enableBottomPadding enableTopPadding>
+      <h1 className="my-4 text-2xl">Tekken 8 flash cards</h1>
+      <section>
+        <h2 className="my-3 text-xl">How it works</h2>
+        <p className="mb-2">
+          A flash card shows a move on the front side. Your job is to guess a
+          property of the move, for example how many frames it is on block. Then
+          you flip the card and check if your guess was correct. Cards marked as
+          "Wrong" will be shown again sooner than cards marked as "Correct".
+          Cards marked as "Ignore" will never be shown again.
+        </p>
+      </section>
+      <CharacterGrid
+        characterCards={gamePageDataT8.characterInfoList.map(
+          ({ id, displayName }) => {
+            const imgSrc = t8AvatarMap[id]
+            return { name: displayName, imgSrc, url: '/t8/' + id + '/flashcard' }
+          },
+        )}
+      />
+    </ContentContainer>
+  )
 }
