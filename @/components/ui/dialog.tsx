7a5b2867045c035a3015fe8ff713b290ce0da7ec
Philip Bruvoll <pbruvoll@hotmail.com>
2024-10-07 21:42:09 +0200

    Support text with links to commands (#199)
    * Support text with links to commands
    
    * format

--- @/components/ui/dialog.tsx
+++ @/components/ui/dialog.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
-
 import { cn } from '@/lib/utils'
 
 const Dialog = DialogPrimitive.Root
@@ -108,13 +107,13 @@ DialogDescription.displayName = DialogPrimitive.Description.displayName
 
 export {
   Dialog,
-  DialogPortal,
-  DialogOverlay,
   DialogClose,
-  DialogTrigger,
   DialogContent,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
   DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
 }
---
+++ app/components/TextWithCommand.tsx
@@ -0,0 +1,33 @@
+import { type Move } from '~/types/Move'
+import { Command } from './Command'
+
+export type TextWithCommandProps = {
+  text: string
+  charUrl: string
+  compressedCommandMap: Record<string, Move>
+}
+export const TextWithCommand = ({
+  text,
+  charUrl,
+  compressedCommandMap,
+}: TextWithCommandProps) => {
+  const splittedText = text.split(/(".*?")/)
+  return (
+    <>
+      {splittedText.map((value, index) => {
+        if (value.startsWith('"') && value.endsWith('"')) {
+          const command = value.slice(1, -1)
+          return (
+            <Command
+              key={index}
+              charUrl={charUrl}
+              command={command}
+              compressedCommandMap={compressedCommandMap}
+            />
+          )
+        }
+        return value
+      })}
+    </>
+  )
+}
