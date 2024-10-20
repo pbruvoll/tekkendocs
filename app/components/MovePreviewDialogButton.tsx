ba8b406cd9b8a6d2f40f526a1222b86c4fb11739
Philip Bruvoll <pbruvoll@hotmail.com>
2024-10-20 16:24:15 +0200

    videoPreviewInModal (#207)
    * videoPreviewInModal
    
    * lint

---
+++ app/components/MovePreviewDialogButton.tsx
@@ -0,0 +1,49 @@
+import { VideoIcon } from '@radix-ui/react-icons'
+import { Link } from '@remix-run/react'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { type Move } from '~/types/Move'
+import { MoveVideo } from './MoveVideo'
+
+export type MovePreviewDialogProps = {
+  move: Move
+  url: string
+}
+export function MovePreviewDialogButton({ move, url }: MovePreviewDialogProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button type="button">
+          <VideoIcon />
+        </button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>
+            <Link className="text-text-primary" to={url}>
+              {move.command}
+            </Link>
+          </DialogTitle>
+          {move.name && <DialogDescription>{move.name}</DialogDescription>}
+        </DialogHeader>
+        <div className="mt-4 max-w-[600px]">
+          <MoveVideo move={move} />
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button">Close</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
