708ec1d65ec494b3242189c198b08b90ae9c728f
Philip Bruvoll <pbruvoll@hotmail.com>
2024-12-01 11:32:28 +0100

    make asuka anti strats nicer (#221)

--- app/utils/commandUtils.test.ts
+++ app/utils/commandUtils.test.ts
@@ -4,4 +4,5 @@ test('compress command', () => {
   expect(compressCommand('d/f+1,2')).toEqual('df1,2')
   expect(compressCommand('CH (1),2,3 (12 ob)')).toEqual('1,2,3')
   expect(compressCommand('CH (1),2,3(12 ob)')).toEqual('1,2,3')
+  expect(compressCommand('f,F+2')).toEqual('ff2')
 })
--- app/utils/commandUtils.ts
+++ app/utils/commandUtils.ts
@@ -1,7 +1,8 @@
 export const compressCommand = (command: string): string => {
   return command
     .replace(/^CH /, '') // remove starting with CH
-    .replace(/\([^)]*\)$/, '') //remove parentheses at the end 1,2 (ob 12) => 1,2
+    .replace(/\([^)]*\)$/, '') //remove parentheses at the end 1,2 (ob 12) => 1,2 (only relevatn for anti start guide)
     .replace(/[ .+/()]/g, '') //remove parentheses (1),2 => 1,2
     .toLowerCase()
+    .replace(/(?<=[a-z]),(?=[a-z])/g, '') //f,f1,2 => ff1,2 // only relevant for anti strat guide
 }
