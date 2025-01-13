5bb98e5c2a5158f21c5521404adfb1f386f74578
Silktail <qwertykeebored@gmail.com>
2025-01-13 11:38:23 -0500

    adjusted for unit tests

--- app/features/flashCards/flashCardFront.tsx
+++ app/features/flashCards/flashCardFront.tsx
@@ -4,7 +4,6 @@ import { type Move, type MoveT8 } from '~/types/Move'
 import { charIdFromMove } from '~/utils/moveUtils'
 import { ShowVideoButton } from './showVideoButton'
 
-
 export type FlashCardFrontProps = {
   move: Move
   showCharName: boolean
@@ -15,12 +14,11 @@ export var toggled: boolean
 
 export function toggleFrontCard(): void {
   if (toggled) {
-    toggled = false;
+    toggled = false
+  } else {
+    toggled = true
   }
-  else {
-    toggled = true;
-  }
-};
+}
 export const FlashCardFront = ({
   move,
   showCharName,
@@ -37,8 +35,7 @@ export const FlashCardFront = ({
           {showCharName && `${charIdFromMove(move as MoveT8)} `}
           {move.command}
         </button>
-        <ShowVideoButton
-          move={move} className="m-1 my-3" hideFrameData />
+        <ShowVideoButton move={move} className="m-1 my-3" hideFrameData />
         <button
           className="flex w-full items-center justify-center p-2 text-lg"
           onClick={onFlip}
@@ -53,11 +50,7 @@ export const FlashCardFront = ({
           </div>
         </button>
       </div>
-    );
+    )
   }
-  return (
-    <div className="flex h-full w-full flex-col bg-foreground/10">
-
-    </div>
-  );
+  return <div className="flex h-full w-full flex-col bg-foreground/10"></div>
 }
