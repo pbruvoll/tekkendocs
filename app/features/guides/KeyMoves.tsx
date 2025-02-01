070de0d0bd1c0a642abe10d57a5a69d709a635aa
Philip Bruvoll <pbruvoll@hotmail.com>
2025-02-01 21:09:14 +0100

    Frame preview guide (#260)
    * test preview
    
    * show move summary in guide

--- app/features/guides/KeyMoves.tsx
+++ app/features/guides/KeyMoves.tsx
@@ -1,6 +1,8 @@
 import { Heading } from '@radix-ui/themes'
 import { Commands } from '~/components/Commands'
 import { TextWithCommand } from '~/components/TextWithCommand'
+import { type Move } from '~/types/Move'
+import { compressCommand } from '~/utils/commandUtils'
 import { useGuideContext } from './GuideContext'
 import { type KeyMove } from './GuideData'
 import { GuideSectionHeading } from './GuideSectionHeading'
@@ -30,8 +32,36 @@ export const KeyMoves = ({ moves, title }: KeyMovesProps) => {
               compressedCommandMap={compressedCommandMap}
             />
           )}
+          <MoveSummary
+            command={command}
+            compressedCommandMap={compressedCommandMap}
+          />
         </section>
       ))}
     </section>
+  )
+}
+
+const MoveSummary = ({
+  command,
+  compressedCommandMap,
+}: {
+  command: string
+  compressedCommandMap: Record<string, Move>
+}) => {
+  const move = compressedCommandMap[compressCommand(command.split(' | ')[0])]
+  if (!move) return null
+  return (
+    <div className="text-sm text-muted-foreground">
+      {[
+        move.hitLevel && move.hitLevel,
+        move.startup && move.startup.split(',')[0],
+        move.block && `${move.block} oB`,
+        move.hit && `${move.hit} oH`,
+        move.counterHit && `${move.counterHit} Oc`,
+      ]
+        .filter(Boolean)
+        .join(', ')}
+    </div>
   )
 }
