5caca208aca5563e8bcf0c4b1f69fc98f9430c9d
Philip Bruvoll <pbruvoll@hotmail.com>
2025-03-01 16:14:03 +0100

    Add branded avatars (#275)
    * Add branded avatars
    
    * fixes

--- app/utils/t8AvatarMap.ts
+++ app/utils/t8AvatarMap.ts
@@ -1,40 +1,77 @@
 import alisa from '~/images/t8/avatars/alisa-128.webp'
+import alisaBrand from '~/images/t8/avatars/alisa-brand-128.webp'
 import asuka from '~/images/t8/avatars/asuka-128.webp'
+import asukaBrand from '~/images/t8/avatars/asuka-brand-128.webp'
 import azucena from '~/images/t8/avatars/azucena-128.webp'
+import azucenaBrand from '~/images/t8/avatars/azucena-brand-128.webp'
 import bryan from '~/images/t8/avatars/bryan-128.webp'
+import bryanBrand from '~/images/t8/avatars/bryan-brand-128.webp'
 import claudio from '~/images/t8/avatars/claudio-128.webp'
+import claudioBrand from '~/images/t8/avatars/claudio-brand-128.webp'
 import clive from '~/images/t8/avatars/clive-128.webp'
+import cliveBrand from '~/images/t8/avatars/clive-brand-128.webp'
 import devilJin from '~/images/t8/avatars/devil-jin-128.webp'
+import devilJinBrand from '~/images/t8/avatars/devil-jin-brand-128.webp'
 import dragunov from '~/images/t8/avatars/dragunov-128.webp'
+import dragunovBrand from '~/images/t8/avatars/dragunov-brand-128.webp'
 import eddy from '~/images/t8/avatars/eddy-128.webp'
+import eddyBrand from '~/images/t8/avatars/eddy-brand-128.webp'
 import feng from '~/images/t8/avatars/feng-128.webp'
+import fengBrand from '~/images/t8/avatars/feng-brand-128.webp'
 import heihachi from '~/images/t8/avatars/heihachi-128.webp'
+import heihachiBrand from '~/images/t8/avatars/heihachi-brand-128.webp'
 import hwoarang from '~/images/t8/avatars/hwoarang-128.webp'
+import hwoarangBrand from '~/images/t8/avatars/hwoarang-brand-128.webp'
 import jack8 from '~/images/t8/avatars/jack-8-128.webp'
+import jack8Brand from '~/images/t8/avatars/jack-8-brand-128.webp'
 import jin from '~/images/t8/avatars/jin-128.webp'
+import jinBrand from '~/images/t8/avatars/jin-brand-128.webp'
 import jun from '~/images/t8/avatars/jun-128.webp'
+import junBrand from '~/images/t8/avatars/jun-brand-128.webp'
 import kazuya from '~/images/t8/avatars/kazuya-128.webp'
+import kazuyaBrand from '~/images/t8/avatars/kazuya-brand-128.webp'
 import king from '~/images/t8/avatars/king-128.webp'
+import kingBrand from '~/images/t8/avatars/king-brand-128.webp'
 import kuma from '~/images/t8/avatars/kuma-128.webp'
+import kumaBrand from '~/images/t8/avatars/kuma-brand-128.webp'
 import lars from '~/images/t8/avatars/lars-128.webp'
+import larsBrand from '~/images/t8/avatars/lars-brand-128.webp'
 import law from '~/images/t8/avatars/law-128.webp'
+import lawBrand from '~/images/t8/avatars/law-brand-128.webp'
 import lee from '~/images/t8/avatars/lee-128.webp'
+import leeBrand from '~/images/t8/avatars/lee-brand-128.webp'
 import leo from '~/images/t8/avatars/leo-128.webp'
+import leoBrand from '~/images/t8/avatars/leo-brand-128.webp'
 import leroy from '~/images/t8/avatars/leroy-128.webp'
+import leroyBrand from '~/images/t8/avatars/leroy-brand-128.webp'
 import lidia from '~/images/t8/avatars/lidia-128.webp'
+import lidiaBrand from '~/images/t8/avatars/lidia-brand-128.webp'
 import lili from '~/images/t8/avatars/lili-128.webp'
+import liliBrand from '~/images/t8/avatars/lili-brand-128.webp'
 import mokujin from '~/images/t8/avatars/mokujin-128.webp'
+import mokujinBrand from '~/images/t8/avatars/mokujin-brand-128.webp'
 import nina from '~/images/t8/avatars/nina-128.webp'
+import ninaBrand from '~/images/t8/avatars/nina-brand-128.webp'
 import panda from '~/images/t8/avatars/panda-128.webp'
+import pandaBrand from '~/images/t8/avatars/panda-brand-128.webp'
 import paul from '~/images/t8/avatars/paul-128.webp'
+import paulBrand from '~/images/t8/avatars/paul-brand-128.webp'
 import raven from '~/images/t8/avatars/raven-128.webp'
+import ravenBrand from '~/images/t8/avatars/raven-brand-128.webp'
 import reina from '~/images/t8/avatars/reina-128.webp'
+import reinaBrand from '~/images/t8/avatars/reina-brand-128.webp'
 import shaheen from '~/images/t8/avatars/shaheen-128.webp'
+import shaheenBrand from '~/images/t8/avatars/shaheen-brand-128.webp'
 import steve from '~/images/t8/avatars/steve-128.webp'
+import steveBrand from '~/images/t8/avatars/steve-brand-128.webp'
 import victor from '~/images/t8/avatars/victor-128.webp'
+import victorBrand from '~/images/t8/avatars/victor-brand-128.webp'
 import xiaoyu from '~/images/t8/avatars/xiaoyu-128.webp'
+import xiaoyuBrand from '~/images/t8/avatars/xiaoyu-brand-128.webp'
 import yoshimitsu from '~/images/t8/avatars/yoshimitsu-128.webp'
+import yoshimitsuBrand from '~/images/t8/avatars/yoshimitsu-brand-128.webp'
 import zafina from '~/images/t8/avatars/zafina-128.webp'
+import zafinaBrand from '~/images/t8/avatars/zafina-brand-128.webp'
 
 export const t8AvatarMap: Record<string, string> = {
   alisa,
@@ -74,4 +111,44 @@ export const t8AvatarMap: Record<string, string> = {
   xiaoyu,
   yoshimitsu,
   zafina,
+}
+
+export const t8AvatarBrandMap: Record<string, string> = {
+  alisa: alisaBrand,
+  asuka: asukaBrand,
+  azucena: azucenaBrand,
+  claudio: claudioBrand,
+  clive: cliveBrand,
+  bryan: bryanBrand,
+  'devil-jin': devilJinBrand,
+  dragunov: dragunovBrand,
+  eddy: eddyBrand,
+  feng: fengBrand,
+  hwoarang: hwoarangBrand,
+  heihachi: heihachiBrand,
+  'jack-8': jack8Brand,
+  jin: jinBrand,
+  jun: junBrand,
+  kazuya: kazuyaBrand,
+  king: kingBrand,
+  kuma: kumaBrand,
+  lars: larsBrand,
+  law: lawBrand,
+  lee: leeBrand,
+  leo: leoBrand,
+  leroy: leroyBrand,
+  lidia: lidiaBrand,
+  lili: liliBrand,
+  mokujin: mokujinBrand,
+  raven: ravenBrand,
+  nina: ninaBrand,
+  reina: reinaBrand,
+  panda: pandaBrand,
+  paul: paulBrand,
+  shaheen: shaheenBrand,
+  steve: steveBrand,
+  victor: victorBrand,
+  xiaoyu: xiaoyuBrand,
+  yoshimitsu: yoshimitsuBrand,
+  zafina: zafinaBrand,
 }
