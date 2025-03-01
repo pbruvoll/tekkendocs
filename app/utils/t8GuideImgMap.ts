0cd20fc77cdf2fd6f0e978295fcbd8016973ab1d
Philip Bruvoll <pbruvoll@hotmail.com>
2025-03-01 22:54:07 +0100

    add guide image lili

---
+++ app/utils/t8GuideImgMap.ts
@@ -0,0 +1,5 @@
+import lili from '~/images/t8/guides/lili-1200.webp'
+
+export const t8GuideImgMap: Record<string, string> = {
+  lili
+}
---
+++ app/utils/t8GuideImgSmallMap.ts
@@ -0,0 +1,5 @@
+import lili from '~/images/t8/guides/lili-600.webp'
+
+export const t8GuideImgSmallMap: Record<string, string> = {
+  lili
+}
