4af34cfe02c3360f064685b3600da94d7a46b40d
Philip Bruvoll <pbruvoll@hotmail.com>
2025-03-02 20:25:09 +0100

    Add new images for all guide (#277)

--- app/utils/t8GuideImgSmallMap.ts
+++ app/utils/t8GuideImgSmallMap.ts
@@ -1,5 +1,17 @@
+import asuka from '~/images/t8/guides/asuka-600.webp'
+import claudio from '~/images/t8/guides/claudio-600.webp'
+import jin from '~/images/t8/guides/jin-600.webp'
+import lidia from '~/images/t8/guides/lidia-600.webp'
 import lili from '~/images/t8/guides/lili-600.webp'
+import raven from '~/images/t8/guides/raven-600.webp'
+import xiaoyu from '~/images/t8/guides/xiaoyu-600.webp'
 
 export const t8GuideImgSmallMap: Record<string, string> = {
   lili,
+  asuka,
+  claudio,
+  jin,
+  lidia,
+  raven,
+  xiaoyu,
 }
