02c39e93839e6db6b6cfc87cf84bebe7f680ede4
Philip Bruvoll <pbruvoll@hotmail.com>
2025-03-04 20:21:46 +0100

    Add Bryan's images to guide image maps (#278)
    * Add Bryan's images to guide image maps
    
    * Add Bryan to the guide image map

--- app/utils/t8GuideImgMap.ts
+++ app/utils/t8GuideImgMap.ts
@@ -1,4 +1,5 @@
 import asuka from '~/images/t8/guides/asuka-1200.webp'
+import bryan from '~/images/t8/guides/bryan-1200.webp'
 import claudio from '~/images/t8/guides/claudio-1200.webp'
 import jin from '~/images/t8/guides/jin-1200.webp'
 import lidia from '~/images/t8/guides/lidia-1200.webp'
@@ -9,6 +10,7 @@ import xiaoyu from '~/images/t8/guides/xiaoyu-1200.webp'
 export const t8GuideImgMap: Record<string, string> = {
   lili,
   asuka,
+  bryan,
   claudio,
   jin,
   lidia,
