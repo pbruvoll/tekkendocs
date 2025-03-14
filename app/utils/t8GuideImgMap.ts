8d2b7f39a3775ce1135686db7541c3a25d4398c5
Philip Bruvoll <pbruvoll@hotmail.com>
2025-03-14 21:36:35 +0100

    og for azucena guide

--- app/utils/t8GuideImgMap.ts
+++ app/utils/t8GuideImgMap.ts
@@ -1,4 +1,5 @@
 import asuka from '~/images/t8/guides/asuka-1200.webp'
+import azucena from '~/images/t8/guides/azucena-1200.webp'
 import bryan from '~/images/t8/guides/bryan-1200.webp'
 import claudio from '~/images/t8/guides/claudio-1200.webp'
 import jin from '~/images/t8/guides/jin-1200.webp'
@@ -8,11 +9,12 @@ import raven from '~/images/t8/guides/raven-1200.webp'
 import xiaoyu from '~/images/t8/guides/xiaoyu-1200.webp'
 
 export const t8GuideImgMap: Record<string, string> = {
-  lili,
   asuka,
+  azucena,
   bryan,
   claudio,
   jin,
+  lili,
   lidia,
   raven,
   xiaoyu,
