715d2edbe44288662b86fbe86ae47dc0935b64e4
Philip Bruvoll <pbruvoll@hotmail.com>
2025-03-22 16:55:13 +0100

    add 256 version of avatars (#287)
    * add 256 version of avatars
    
    * remove unnecessary newline in t8AvatarBrandMap256
    
    * remove unused import of t8AvatarMap in main layout

---
+++ app/utils/t8AvatarBrandMap256.ts
@@ -0,0 +1,77 @@
+import alisa from '~/images/t8/avatars/alisa-brand-256.webp'
+import asuka from '~/images/t8/avatars/asuka-brand-256.webp'
+import azucena from '~/images/t8/avatars/azucena-brand-256.webp'
+import bryan from '~/images/t8/avatars/bryan-brand-256.webp'
+import claudio from '~/images/t8/avatars/claudio-brand-256.webp'
+import clive from '~/images/t8/avatars/clive-brand-256.webp'
+import devilJin from '~/images/t8/avatars/devil-jin-brand-256.webp'
+import dragunov from '~/images/t8/avatars/dragunov-brand-256.webp'
+import eddy from '~/images/t8/avatars/eddy-brand-256.webp'
+import feng from '~/images/t8/avatars/feng-brand-256.webp'
+import heihachi from '~/images/t8/avatars/heihachi-brand-256.webp'
+import hwoarang from '~/images/t8/avatars/hwoarang-brand-256.webp'
+import jack8 from '~/images/t8/avatars/jack-8-brand-256.webp'
+import jin from '~/images/t8/avatars/jin-brand-256.webp'
+import jun from '~/images/t8/avatars/jun-brand-256.webp'
+import kazuya from '~/images/t8/avatars/kazuya-brand-256.webp'
+import king from '~/images/t8/avatars/king-brand-256.webp'
+import kuma from '~/images/t8/avatars/kuma-brand-256.webp'
+import lars from '~/images/t8/avatars/lars-brand-256.webp'
+import law from '~/images/t8/avatars/law-brand-256.webp'
+import lee from '~/images/t8/avatars/lee-brand-256.webp'
+import leo from '~/images/t8/avatars/leo-brand-256.webp'
+import leroy from '~/images/t8/avatars/leroy-brand-256.webp'
+import lidia from '~/images/t8/avatars/lidia-brand-256.webp'
+import lili from '~/images/t8/avatars/lili-brand-256.webp'
+import mokujin from '~/images/t8/avatars/mokujin-brand-256.webp'
+import nina from '~/images/t8/avatars/nina-brand-256.webp'
+import panda from '~/images/t8/avatars/panda-brand-256.webp'
+import paul from '~/images/t8/avatars/paul-brand-256.webp'
+import raven from '~/images/t8/avatars/raven-brand-256.webp'
+import reina from '~/images/t8/avatars/reina-brand-256.webp'
+import shaheen from '~/images/t8/avatars/shaheen-brand-256.webp'
+import steve from '~/images/t8/avatars/steve-brand-256.webp'
+import victor from '~/images/t8/avatars/victor-brand-256.webp'
+import xiaoyu from '~/images/t8/avatars/xiaoyu-brand-256.webp'
+import yoshimitsu from '~/images/t8/avatars/yoshimitsu-brand-256.webp'
+import zafina from '~/images/t8/avatars/zafina-brand-256.webp'
+
+export const t8AvatarBrandMap256: Record<string, string> = {
+  alisa,
+  asuka,
+  azucena,
+  claudio,
+  clive,
+  bryan,
+  'devil-jin': devilJin,
+  dragunov,
+  eddy,
+  feng,
+  hwoarang,
+  heihachi,
+  'jack-8': jack8,
+  jin,
+  jun,
+  kazuya,
+  king,
+  kuma,
+  lars,
+  law,
+  lee,
+  leo,
+  leroy,
+  lidia,
+  lili,
+  mokujin,
+  raven,
+  nina,
+  reina,
+  panda,
+  paul,
+  shaheen,
+  steve,
+  victor,
+  xiaoyu,
+  yoshimitsu,
+  zafina,
+}
