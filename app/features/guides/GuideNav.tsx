6eb1e023bbcd04f16d90279baf2b018bce5c64ce
Philip Bruvoll <pbruvoll@hotmail.com>
2025-03-29 19:51:28 +0100

    feat: add installments section to guides navigation and update Installments component

--- app/features/guides/GuideNav.tsx
+++ app/features/guides/GuideNav.tsx
@@ -11,6 +11,7 @@ export const GuideNav = ({ guideData }: GuideNavPros) => {
     !!guideData.strengths && 'Strengths',
     !!guideData.weaknesses && 'Weaknesses',
     !!guideData.heatSystem && 'Heat System',
+    !!guideData.installments && 'Installments',
     !!guideData.keyMoves && 'Top 10 Moves',
     !!guideData.standingPunishers && 'Punishers',
     !!guideData.combos && 'Combos',
