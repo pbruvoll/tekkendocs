600e9bc35b1fc865e4807c302c503126b653db94
Philip Bruvoll <pbruvoll@hotmail.com>
2025-05-17 21:52:05 +0200

    format

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
   plugins: [
     remixDevTools(),
     remix({
-      future : {
+      future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-      }
+      },
     }),
     tsconfigPaths(),
     sentryVitePlugin({
