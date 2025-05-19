5325e5f7bf98cc72bcd623149e282e5f162968b6
Philip Bruvoll <pbruvoll@hotmail.com>
2025-05-19 21:09:51 +0200

    revert

--- app/components/FrameDataFilterDialog.tsx
+++ app/components/FrameDataFilterDialog.tsx
@@ -17,14 +17,13 @@ export const FrameDataFilterDialog = ({
   states,
   transitions,
 }: FrameDataFilterDialogProps) => {
-  const filterCount = Object.entries(filter).filter(([k, value]) => {
+  const filterCount = Object.entries(filter).filter(([, value]) => {
     if (value === undefined) {
       return false
     }
     if (value instanceof Array) {
       return value.length > 0
     }
-    console.log(k, ' ', value)
     return true
   }).length
 
