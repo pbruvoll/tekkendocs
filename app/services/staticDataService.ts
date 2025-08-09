4a037acb36298bfa3ef221eac8445b6e51684a70
Philip Bruvoll <pbruvoll@hotmail.com>
2025-08-09 18:33:06 +0200

    eddy-guide

--- app/services/staticDataService.ts
+++ app/services/staticDataService.ts
@@ -115,6 +115,7 @@ export const characterGuideAuthors: Record<
     bryan: { author: 'Ty' },
     claudio: { author: 'Tetsu', version: 'S2' },
     'devil-jin': { author: 'Vermilion', version: 'S2' },
+    eddy: { author: 'Ace', version: 'S2' },
     hwoarang: { author: 'Kicking Macine', version: 'S2' },
     jin: { author: 'Bok Dibi', version: 'S2' },
     jun: { author: 'Pagani', version: 'S2' },
