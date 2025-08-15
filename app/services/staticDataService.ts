8f1c4d9564e4d64e41a079c9d7ff503464146bad
Philip Bruvoll <pbruvoll@hotmail.com>
2025-08-15 20:29:52 +0200

    bryan s2

--- app/services/staticDataService.ts
+++ app/services/staticDataService.ts
@@ -112,7 +112,7 @@ export const characterGuideAuthors: Record<
   T8: {
     asuka: { author: 'Fergus', version: 'S2' },
     azucena: { author: 'Koenji' },
-    bryan: { author: 'Ty' },
+    bryan: { author: 'Ty', version: 'S2' },
     claudio: { author: 'Tetsu', version: 'S2' },
     'devil-jin': { author: 'Vermilion', version: 'S2' },
     dragunov: { author: 'Fear of Silence', version: 'S2' },
