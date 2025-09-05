4c2eab5760be69d4449042d2a3a2980eefba6529
Philip Bruvoll <pbruvoll@hotmail.com>
2025-09-05 16:30:40 +0200

    Format duckableString parameter retrieval for improved readability

--- app/utils/filterUtils.ts
+++ app/utils/filterUtils.ts
@@ -44,7 +44,10 @@ export const getFilterFromParams = (
     tornado: getSearchParamBoolean(searchParams, filterKey.Tornado),
     jails: getSearchParamBoolean(searchParams, filterKey.Jails),
     noJails: getSearchParamBoolean(searchParams, filterKey.NoJails),
-    duckableString: getSearchParamBoolean(searchParams, filterKey.DuckableString),
+    duckableString: getSearchParamBoolean(
+      searchParams,
+      filterKey.DuckableString,
+    ),
     numHitsMin: getSearchParamNumber(searchParams, filterKey.NumHitsMin),
     numHitsMax: getSearchParamNumber(searchParams, filterKey.NumHitsMax),
     stance: getSearchParamStringList(searchParams, filterKey.Stance),
