14e61b0071e1dd87b8844c79fa2836e34de018b6
Philip Bruvoll <pbruvoll@hotmail.com>
2025-09-06 21:40:37 +0200

    refactor: clean up imports in badge, switch, and table components; update rank images in main layout

--- @/components/ui/table.tsx
+++ @/components/ui/table.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-
 import { cn } from '@/lib/utils'
 
 const Table = React.forwardRef<
@@ -107,11 +106,11 @@ TableCaption.displayName = 'TableCaption'
 
 export {
   Table,
-  TableHeader,
   TableBody,
+  TableCaption,
+  TableCell,
   TableFooter,
   TableHead,
+  TableHeader,
   TableRow,
-  TableCell,
-  TableCaption,
 }
