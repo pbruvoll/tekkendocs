4f18f1955e442ace21e89023ffcebaa06cb6bae3
Philip Bruvoll <pbruvoll@hotmail.com>
2025-10-05 18:52:07 +0200

    add guide image for anna, fahkumram and lars

--- app/utils/t8GuideImgMap.ts
+++ app/utils/t8GuideImgMap.ts
@@ -1,4 +1,4 @@
-import anna from '~/images/t8/avatars/anna-512.webp'
+import anna from '~/images/t8/guides/anna-1200.webp'
 import asuka from '~/images/t8/guides/asuka-1200.webp'
 import azucena from '~/images/t8/guides/azucena-1200.webp'
 import bryan from '~/images/t8/guides/bryan-1200.webp'
@@ -6,9 +6,11 @@ import claudio from '~/images/t8/guides/claudio-1200.webp'
 import devilJin from '~/images/t8/guides/devil-jin-1200.webp'
 import dragunov from '~/images/t8/guides/dragunov-1200.webp'
 import eddy from '~/images/t8/guides/eddy-1200.webp'
+import fahkumram from '~/images/t8/guides/fahkumram-1200.webp'
 import hwoarang from '~/images/t8/guides/hwoarang-1200.webp'
 import jin from '~/images/t8/guides/jin-1200.webp'
 import jun from '~/images/t8/guides/jun-1200.webp'
+import lars from '~/images/t8/guides/lars-1200.webp'
 import lee from '~/images/t8/guides/lee-1200.webp'
 import lidia from '~/images/t8/guides/lidia-1200.webp'
 import lili from '~/images/t8/guides/lili-1200.webp'
@@ -26,7 +28,9 @@ export const t8GuideImgMap: Record<string, string> = {
   'devil-jin': devilJin,
   dragunov,
   eddy,
+  fahkumram,
   hwoarang,
+  lars,
   jin,
   jun,
   lee,
--- app/utils/t8GuideImgSmallMap.ts
+++ app/utils/t8GuideImgSmallMap.ts
@@ -1,4 +1,4 @@
-import anna from '~/images/t8/avatars/anna-512.webp'
+import anna from '~/images/t8/guides/anna-600.webp'
 import asuka from '~/images/t8/guides/asuka-600.webp'
 import azucena from '~/images/t8/guides/azucena-600.webp'
 import bryan from '~/images/t8/guides/bryan-600.webp'
@@ -6,9 +6,11 @@ import claudio from '~/images/t8/guides/claudio-600.webp'
 import devilJin from '~/images/t8/guides/devil-jin-600.webp'
 import dragunov from '~/images/t8/guides/dragunov-600.webp'
 import eddy from '~/images/t8/guides/eddy-600.webp'
+import fahkumram from '~/images/t8/guides/fahkumram-600.webp'
 import hwoarang from '~/images/t8/guides/hwoarang-600.webp'
 import jin from '~/images/t8/guides/jin-600.webp'
 import jun from '~/images/t8/guides/jun-600.webp'
+import lars from '~/images/t8/guides/lars-600.webp'
 import lee from '~/images/t8/guides/lee-600.webp'
 import lidia from '~/images/t8/guides/lidia-600.webp'
 import lili from '~/images/t8/guides/lili-600.webp'
@@ -26,9 +28,11 @@ export const t8GuideImgSmallMap: Record<string, string> = {
   'devil-jin': devilJin,
   dragunov,
   eddy,
+  fahkumram,
   hwoarang,
   jin,
   jun,
+  lars,
   lee,
   lidia,
   lili,
