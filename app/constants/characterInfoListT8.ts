8314051ccaefb515a838c4b471778a882c90fd36
Philip Bruvoll <pbruvoll@hotmail.com>
2025-11-02 22:27:01 +0100

    format

--- app/constants/characterInfoListT8.ts
+++ app/constants/characterInfoListT8.ts
@@ -1,244 +1,243 @@
-type CharacterInfoT8 ={
+type CharacterInfoT8 = {
   id: string;
   displayName: string;
   wavuName: string;
-  aliasList: string[]; 
-}
-
+  aliasList: string[];
+};
 
 export const characterInfoT8List: CharacterInfoT8[] = [
   {
-    id: "alisa",
-    displayName: "Alisa",
-    wavuName: "Alisa",
-    aliasList: ["ali", "als"]
+    id: 'alisa',
+    displayName: 'Alisa',
+    wavuName: 'Alisa',
+    aliasList: ['ali', 'als'],
   },
   {
-    id: "anna",
-    displayName: "Anna",
-    wavuName: "Anna",
-    aliasList: ["ana", "an"]
+    id: 'anna',
+    displayName: 'Anna',
+    wavuName: 'Anna',
+    aliasList: ['ana', 'an'],
   },
   {
-    id: "armor-king",
-    displayName: "Armor King",
-    wavuName: "Armor King",
-    aliasList: ["ak"]
+    id: 'armor-king',
+    displayName: 'Armor King',
+    wavuName: 'Armor King',
+    aliasList: ['ak'],
   },
   {
-    id: "asuka",
-    displayName: "Asuka",
-    wavuName: "Asuka",
-    aliasList: ["oscar", "oskar", "aska"]
+    id: 'asuka',
+    displayName: 'Asuka',
+    wavuName: 'Asuka',
+    aliasList: ['oscar', 'oskar', 'aska'],
   },
   {
-    id: "azucena",
-    displayName: "Azucena",
-    wavuName: "Azucena",
-    aliasList: ["azu"]
+    id: 'azucena',
+    displayName: 'Azucena',
+    wavuName: 'Azucena',
+    aliasList: ['azu'],
   },
   {
-    id: "bryan",
-    displayName: "Bryan",
-    wavuName: "Bryan",
-    aliasList: ["bry", "byron"]
+    id: 'bryan',
+    displayName: 'Bryan',
+    wavuName: 'Bryan',
+    aliasList: ['bry', 'byron'],
   },
   {
-    id: "claudio",
-    displayName: "Claudio",
-    wavuName: "Claudio",
-    aliasList: ["cld", "cla", "claud"]
+    id: 'claudio',
+    displayName: 'Claudio',
+    wavuName: 'Claudio',
+    aliasList: ['cld', 'cla', 'claud'],
   },
   {
-    id: "clive",
-    displayName: "Clive",
-    wavuName: "Clive",
-    aliasList: ["cli", "clv"]
+    id: 'clive',
+    displayName: 'Clive',
+    wavuName: 'Clive',
+    aliasList: ['cli', 'clv'],
   },
   {
-    id: "devil-jin",
-    displayName: "Devil Jin",
-    wavuName: "Devil Jin",
-    aliasList: ["dvj", "dj", "deviljin", "djin"]
+    id: 'devil-jin',
+    displayName: 'Devil Jin',
+    wavuName: 'Devil Jin',
+    aliasList: ['dvj', 'dj', 'deviljin', 'djin'],
   },
   {
-    id: "dragunov",
-    displayName: "Dragunov",
-    wavuName: "Dragunov",
-    aliasList: ["dra", "drag", "dragu", "sergei"]
+    id: 'dragunov',
+    displayName: 'Dragunov',
+    wavuName: 'Dragunov',
+    aliasList: ['dra', 'drag', 'dragu', 'sergei'],
   },
   {
-    id: "eddy",
-    displayName: "Eddy",
-    wavuName: "Eddy",
-    aliasList: ["ed", "capo"]
+    id: 'eddy',
+    displayName: 'Eddy',
+    wavuName: 'Eddy',
+    aliasList: ['ed', 'capo'],
   },
   {
-    id: "fahkumram",
-    displayName: "Fahkumram",
-    wavuName: "Fahkumram",
-    aliasList: ["fahk", "fak", "fah"]
+    id: 'fahkumram',
+    displayName: 'Fahkumram',
+    wavuName: 'Fahkumram',
+    aliasList: ['fahk', 'fak', 'fah'],
   },
   {
-    id: "feng",
-    displayName: "Feng",
-    wavuName: "Feng",
-    aliasList: ["fen"]
+    id: 'feng',
+    displayName: 'Feng',
+    wavuName: 'Feng',
+    aliasList: ['fen'],
   },
   {
-    id: "hwoarang",
-    displayName: "Hwoarang",
-    wavuName: "Hwoarang",
-    aliasList: ["hwo"]
+    id: 'hwoarang',
+    displayName: 'Hwoarang',
+    wavuName: 'Hwoarang',
+    aliasList: ['hwo'],
   },
   {
-    id: "heihachi",
-    displayName: "Heihachi",
-    wavuName: "Heihachi",
-    aliasList: ["hei", "hachi"]
+    id: 'heihachi',
+    displayName: 'Heihachi',
+    wavuName: 'Heihachi',
+    aliasList: ['hei', 'hachi'],
   },
   {
-    id: "jack-8",
-    displayName: "Jack-8",
-    wavuName: "Jack-8",
-    aliasList: ["jack", "j8", "jack8"]
+    id: 'jack-8',
+    displayName: 'Jack-8',
+    wavuName: 'Jack-8',
+    aliasList: ['jack', 'j8', 'jack8'],
   },
   {
-    id: "jin",
-    displayName: "Jin",
-    wavuName: "Jin",
-    aliasList: ["jim"]
+    id: 'jin',
+    displayName: 'Jin',
+    wavuName: 'Jin',
+    aliasList: ['jim'],
   },
   {
-    id: "jun",
-    displayName: "Jun",
-    wavuName: "Jun",
-    aliasList: []
+    id: 'jun',
+    displayName: 'Jun',
+    wavuName: 'Jun',
+    aliasList: [],
   },
   {
-    id: "kazuya",
-    displayName: "Kazuya",
-    wavuName: "Kazuya",
-    aliasList: ["kaz", "masku"]
+    id: 'kazuya',
+    displayName: 'Kazuya',
+    wavuName: 'Kazuya',
+    aliasList: ['kaz', 'masku'],
   },
   {
-    id: "king",
-    displayName: "King",
-    wavuName: "King",
-    aliasList: []
+    id: 'king',
+    displayName: 'King',
+    wavuName: 'King',
+    aliasList: [],
   },
   {
-    id: "kuma",
-    displayName: "Kuma",
-    wavuName: "Kuma",
-    aliasList: ["bear"]
+    id: 'kuma',
+    displayName: 'Kuma',
+    wavuName: 'Kuma',
+    aliasList: ['bear'],
   },
   {
-    id: "lars",
-    displayName: "Lars",
-    wavuName: "Lars",
-    aliasList: []
+    id: 'lars',
+    displayName: 'Lars',
+    wavuName: 'Lars',
+    aliasList: [],
   },
   {
-    id: "law",
-    displayName: "Law",
-    wavuName: "Law",
-    aliasList: ["marshall"]
+    id: 'law',
+    displayName: 'Law',
+    wavuName: 'Law',
+    aliasList: ['marshall'],
   },
   {
-    id: "lee",
-    displayName: "Lee",
-    wavuName: "Lee",
-    aliasList: ["violet"]
+    id: 'lee',
+    displayName: 'Lee',
+    wavuName: 'Lee',
+    aliasList: ['violet'],
   },
   {
-    id: "leo",
-    displayName: "Leo",
-    wavuName: "Leo",
-    aliasList: []
+    id: 'leo',
+    displayName: 'Leo',
+    wavuName: 'Leo',
+    aliasList: [],
   },
   {
-    id: "leroy",
-    displayName: "Leroy",
-    wavuName: "Leroy",
-    aliasList: ["ler", "roy"]
+    id: 'leroy',
+    displayName: 'Leroy',
+    wavuName: 'Leroy',
+    aliasList: ['ler', 'roy'],
   },
   {
-    id: "lidia",
-    displayName: "Lidia",
-    wavuName: "Lidia",
-    aliasList: ["lid", "pm"]
+    id: 'lidia',
+    displayName: 'Lidia',
+    wavuName: 'Lidia',
+    aliasList: ['lid', 'pm'],
   },
   {
-    id: "lili",
-    displayName: "Lili",
-    wavuName: "Lili",
-    aliasList: ["lil", "riri"]
+    id: 'lili',
+    displayName: 'Lili',
+    wavuName: 'Lili',
+    aliasList: ['lil', 'riri'],
   },
   {
-    id: "nina",
-    displayName: "Nina",
-    wavuName: "Nina",
-    aliasList: []
+    id: 'nina',
+    displayName: 'Nina',
+    wavuName: 'Nina',
+    aliasList: [],
   },
   {
-    id: "panda",
-    displayName: "Panda",
-    wavuName: "Panda",
-    aliasList: ["pan"]
+    id: 'panda',
+    displayName: 'Panda',
+    wavuName: 'Panda',
+    aliasList: ['pan'],
   },
   {
-    id: "paul",
-    displayName: "Paul",
-    wavuName: "Paul",
-    aliasList: []
+    id: 'paul',
+    displayName: 'Paul',
+    wavuName: 'Paul',
+    aliasList: [],
   },
   {
-    id: "raven",
-    displayName: "Raven",
-    wavuName: "Raven",
-    aliasList: ["maven", "masterraven", "mrv"]
+    id: 'raven',
+    displayName: 'Raven',
+    wavuName: 'Raven',
+    aliasList: ['maven', 'masterraven', 'mrv'],
   },
   {
-    id: "reina",
-    displayName: "Reina",
-    wavuName: "Reina",
-    aliasList: ["rei"]
+    id: 'reina',
+    displayName: 'Reina',
+    wavuName: 'Reina',
+    aliasList: ['rei'],
   },
   {
-    id: "shaheen",
-    displayName: "Shaheen",
-    wavuName: "Shaheen",
-    aliasList: ["sha", "heen"]
+    id: 'shaheen',
+    displayName: 'Shaheen',
+    wavuName: 'Shaheen',
+    aliasList: ['sha', 'heen'],
   },
   {
-    id: "steve",
-    displayName: "Steve",
-    wavuName: "Steve",
-    aliasList: ["stv", "fox"]
+    id: 'steve',
+    displayName: 'Steve',
+    wavuName: 'Steve',
+    aliasList: ['stv', 'fox'],
   },
   {
-    id: "victor",
-    displayName: "Victor",
-    wavuName: "Victor",
-    aliasList: ["vic"]
+    id: 'victor',
+    displayName: 'Victor',
+    wavuName: 'Victor',
+    aliasList: ['vic'],
   },
   {
-    id: "xiaoyu",
-    displayName: "Xiaoyu",
-    wavuName: "Xiaoyu",
-    aliasList: ["ling"]
+    id: 'xiaoyu',
+    displayName: 'Xiaoyu',
+    wavuName: 'Xiaoyu',
+    aliasList: ['ling'],
   },
   {
-    id: "yoshimitsu",
-    displayName: "Yoshimitsu",
-    wavuName: "Yoshimitsu",
-    aliasList: ["yoshi"]
+    id: 'yoshimitsu',
+    displayName: 'Yoshimitsu',
+    wavuName: 'Yoshimitsu',
+    aliasList: ['yoshi'],
   },
   {
-    id: "zafina",
-    displayName: "Zafina",
-    wavuName: "Zafina",
-    aliasList: ["zaf"]
-  }
-]
+    id: 'zafina',
+    displayName: 'Zafina',
+    wavuName: 'Zafina',
+    aliasList: ['zaf'],
+  },
+];
