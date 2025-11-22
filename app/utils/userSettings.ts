3871edd8d94a1fa97276d9ae437741b3d1c27f76
Philip Bruvoll <pbruvoll@hotmail.com>
2025-11-22 22:38:23 +0100

    Refactor userSettings.ts for consistent formatting and style

--- app/utils/userSettings.ts
+++ app/utils/userSettings.ts
@@ -1,12 +1,13 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
-export type FrameDataViewMode = 'default' | 'simple'
+export type FrameDataViewMode = 'default' | 'simple';
 export type UserSettings = {
-  frameDataViewMode: FrameDataViewMode,
-  setFrameDataViewMode: (frameDataViewMode: FrameDataViewMode) => void,
-}
+  frameDataViewMode: FrameDataViewMode;
+  setFrameDataViewMode: (frameDataViewMode: FrameDataViewMode) => void;
+};
 
 export const useUserSettings = create<UserSettings>((set) => ({
   frameDataViewMode: 'default',
-  setFrameDataViewMode: (frameDataViewMode: FrameDataViewMode) => set({ frameDataViewMode }),
-}))
+  setFrameDataViewMode: (frameDataViewMode: FrameDataViewMode) =>
+    set({ frameDataViewMode }),
+}));
