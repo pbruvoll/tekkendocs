5d4e817fafa8f003820ddab2a2ac9138d71368c0
Philip Bruvoll <pbruvoll@hotmail.com>
2025-11-22 22:41:13 +0100

    State management (#405)
    * Add zustand to store view settings
    
    * Add devtoolsJson
    
    * Refactor userSettings.ts for consistent formatting and style

---
+++ app/utils/userSettings.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+export type FrameDataViewMode = 'default' | 'simple';
+export type UserSettings = {
+  frameDataViewMode: FrameDataViewMode;
+  setFrameDataViewMode: (frameDataViewMode: FrameDataViewMode) => void;
+};
+
+export const useUserSettings = create<UserSettings>((set) => ({
+  frameDataViewMode: 'default',
+  setFrameDataViewMode: (frameDataViewMode: FrameDataViewMode) =>
+    set({ frameDataViewMode }),
+}));
