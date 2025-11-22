dc0bcdd12a080c5b863bba2c06a467e5046b1328
Philip Bruvoll <pbruvoll@hotmail.com>
2025-11-22 22:34:39 +0100

    Add devtoolsJson

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { reactRouter } from '@react-router/dev/vite';
 //import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    devtoolsJson(),
     reactRouter(),
     tsconfigPaths(),
     // sentryVitePlugin({
