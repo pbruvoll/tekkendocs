6712ee227b412caa43ccd03e43a0b0822ea54e0f
Philip Bruvoll <pbruvoll@hotmail.com>
2026-02-14 22:13:45 +0100

    Refactor code structure for improved readability and maintainability (#453)

--- app/services/staticDataService.ts
+++ app/services/staticDataService.ts
@@ -190,6 +190,7 @@ export const characterGuideAuthors: Record<
     lidia: { author: 'Leemishima', version: 'S2' },
     lili: { author: 'DewGlider', version: 'S2' },
     raven: { author: 'JacobKaas', version: 'S2' },
+    steve: { author: 'Seagerr', version: 'S2' },
     victor: { author: 'Yung_P', version: 'S2' },
     yoshimitsu: { author: 'Trizzy the Rapper', version: 'S2' },
     xiaoyu: { author: 'Kanda', version: 'S2' },
