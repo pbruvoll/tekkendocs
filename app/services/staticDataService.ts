0ccaa75ba2c9ac9d1ebac80137adf66382d3d887
Philip Bruvoll <pbruvoll@hotmail.com>
2026-02-26 13:26:55 +0100

    format

--- app/services/staticDataService.ts
+++ app/services/staticDataService.ts
@@ -204,7 +204,7 @@ export const characterGuideAuthors: Record<
 
 export const siteUrl = 'https://tekkendocs.com';
 
-export const cdnUrl = "https://tekkendocs.b-cdn.net";
+export const cdnUrl = 'https://tekkendocs.b-cdn.net';
 
 export const internalMoveVideoSet = new Set(['reina', 'jun']);
 
