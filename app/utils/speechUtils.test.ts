cc9d0020a87e33aa809a1cced750d41a483505f3
Philip Bruvoll <pbruvoll@hotmail.com>
2026-05-07 22:20:14 +0200

    chore: update vitest to version 4.1.5 and refactor vitest configuration (#531)
    * chore: update vitest to version 4.1.5 and refactor vitest configuration
    
    - Updated vitest dependency in package.json from ^0.34.6 to ^4.1.5.
    - Refactored vitest.config.ts to use tsconfigPaths in the resolve section instead of plugins.
    
    * format

--- app/utils/speechUtils.test.ts
+++ app/utils/speechUtils.test.ts
@@ -5,4 +5,5 @@ test('replaces commas between numbers with spaces', () => {
   expect(normalizeSpeechText('1,2')).toBe('1 2');
   expect(normalizeSpeechText('1, 2')).toBe('1 2');
   expect(normalizeSpeechText('1 ,2')).toBe('1 2');
+  expect(normalizeSpeechText('df1 , 2, 3')).toBe('df1 2 3');
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,15 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    watchExclude: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*'],
   },
 });
